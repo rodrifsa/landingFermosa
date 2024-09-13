6a182d5e1f6a7d3feb5c92d45cc1e4724c84eb32
Guillermo Gonzalez <2808guille@gmail.com>
2024-09-13 19:23:56 -0300

    Landing Page

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style/style.css">
+
+
+    <!-- DESCOMENTAR PARA VER -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous"> -->
+
+</head>
+
+<body>
+
+    <?php
+    include 'templates/nav.php';
+    ?>
+
+
+    <!-- DESCOMENTAR PARA VER -->
+    <!-- <div class="swiffy-slider">
+        <ul class="slider-container">
+            <li><img src="images/slider/image1.jpg" style="max-width: 100%;height: auto;"></li>
+            <li><img src="images/slider/image2.jpg" style="max-width: 100%;height: auto;"></li>
+            <li><img src="images/slider/image3.jpg" style="max-width: 100%;height: auto;"></li>
+        </ul>
+
+        <button type="button" class="slider-nav"></button>
+        <button type="button" class="slider-nav slider-nav-next"></button>
+
+        <div class="slider-indicators">
+            <button class="active"></button>
+            <button></button>
+            <button></button>
+        </div>
+    </div> -->
+
+</body>
+
+</html>
