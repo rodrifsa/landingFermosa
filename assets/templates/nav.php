b363d4cc37eeaefd06237916e1643b9c484a6077
Guillermo Gonzalez <2808guille@gmail.com>
2024-09-20 08:39:45 -0300

    Nuevo Diseño

---
+++ assets/templates/nav.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '
+
+    <header>
+        <a href="index.php" class="logo"><img src="assets/img/logo/logo.png" alt=""></a>
+
+        <ul class="navbar">
+            <li><a href="#" class="active">Nosotros</a></li>
+            <li><a href="#">Carreras</a></li>
+            <li><a href="#">Postgrados</a></li>
+            <li><a href="#">Inscribete</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+
+        <div class="main">
+            <a href="http://fermosainstituto.com/campus" class="user" target="_blank"><button>Campus Virtual</button></a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>
+
+    </header>
+
+</html>
+';
--- index.php
+++ index.php
@@ -6,40 +6,106 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instituto Superior Fermosa</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="style/style.css">
+    <!-- ============================== -->
+    <!-- =========== FAVICON ==========-->
+    <!-- ============================== -->
 
+    <link rel="icon" href="assets/img/icon/icon.png" type="image/x-icon">
 
-    <!-- DESCOMENTAR PARA VER -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous" defer></script>
-    <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous"> -->
+    <!-- ============================== -->
+    <!-- =========== STYLE ============-->
+    <!-- ============================== -->
+
+    <link rel="stylesheet" href="assets/styles/style.css">
+
+    <!-- ============================== -->
+    <!-- =========== ICONS ============-->
+    <!-- ============================== -->
+
+    <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
+        rel="stylesheet" />
+
+    <link rel="stylesheet"
+        href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <!-- ============================== -->
+    <!-- =========== FONTS ============-->
+    <!-- ============================== -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;500;600&display=swap" rel="stylesheet">
 
 </head>
 
 <body>
 
+    <!-- ============================== -->
+    <!-- =========== HEADER ============-->
+    <!-- ============================== -->
+
     <?php
-    include 'templates/nav.php';
+    include('assets/templates/nav.php');
     ?>
 
+    <!-- ============================== -->
+    <!-- =========== SLIDER ===========-->
+    <!-- ============================== -->
 
-    <!-- DESCOMENTAR PARA VER -->
-    <!-- <div class="swiffy-slider">
-        <ul class="slider-container">
-            <li><img src="images/slider/image1.jpg" style="max-width: 100%;height: auto;"></li>
-            <li><img src="images/slider/image2.jpg" style="max-width: 100%;height: auto;"></li>
-            <li><img src="images/slider/image3.jpg" style="max-width: 100%;height: auto;"></li>
-        </ul>
-
-        <button type="button" class="slider-nav"></button>
-        <button type="button" class="slider-nav slider-nav-next"></button>
-
-        <div class="slider-indicators">
-            <button class="active"></button>
-            <button></button>
-            <button></button>
+    <div class="slider">
+        <div class="list">
+            <div class="item">
+                <img src="assets/img/slider/image1.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/slider/image2.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/slider/image3.jpg" alt="">
+            </div>
+            <!-- <div class="item">
+                <img src="img/4.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/5.jpg" alt="">
+            </div> -->
         </div>
-    </div> -->
+        <div class="buttons">
+            <button id="prev">
+                < </button>
+                    <button id="next">></button>
+        </div>
+        <ul class="dots">
+            <li class="active"></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+
+    <!-- ============================== -->
+    <!-- =========== ABOUT US =========-->
+    <!-- ============================== -->
+
+    <div class="about-section">
+        <div class="inner-container">
+            <h1>About us</h1>
+            <p class="text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, optio illum! Temporibus praesentium repellat mollitia eos deserunt, minima quisquam quae veniam non maiores ullam? A est iste ad modi quis.
+            </p>
+            <div class="skills">
+                <span>Web Design</span>
+                <span>Photoshop & Illustrator</span>
+                <span>Coding</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- ============================== -->
+    <!-- =========== SCRIPTS ==========-->
+    <!-- ============================== -->
+
+    <script src="assets/js/script.js" defer></script>
 
 </body>
 
